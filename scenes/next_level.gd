fdc0b793802e0e72f86025d8f1224586009014b6
CarlosNegron-prog <carlos.negron42@upr.edu>
2026-03-30 14:24:30 -0400

    slowed triangles, fixed chest interaction

--- scenes/next_level.gd
+++ scenes/next_level.gd
@@ -1,33 +1,35 @@
 extends Node2D
 
-
 @export var finish_scene: String = "res://scenes/GameIntro.tscn"
 
 var chest_opened := false
 var door_used := false
 
 @onready var chest_interactable: Area2D = $Chest/interactable
-@onready var chest_sprite: Sprite2D = $Chest/ChestSprite
+@onready var chest_sprite: Sprite2D = $Chest/Sprite2D
 @onready var exit_door: StaticBody2D = $ExitDoor
+@onready var hint_label: Label = $UI/HintLabel
 
 func _ready() -> void:
 	add_to_group("game_manager")
 	chest_interactable.interact = _on_chest_interact
 	chest_interactable.interact_name = "Open Chest"
 	chest_interactable.is_interactable = true
+	hint_label.text = "Find and open the chest."
 
 func _on_chest_interact() -> void:
 	if chest_opened:
 		return
-		
-		
+
 	chest_opened = true
 	chest_interactable.is_interactable = false
 	chest_interactable.interact_name = "Chest (Opened)"
 
-# Optional visual swap
-	chest_sprite.texture = load("res://asset/map/chest/chest_open.png")
+	var open_tex := load("res://map/chest/chest_open.png")
+	if chest_sprite and open_tex:
+		chest_sprite.texture = open_tex
 
-# Unlock the exit door now
 	if exit_door.has_method("unlock"):
 		exit_door.unlock()
+
+	hint_label.text = "Door unlocked. Go to the exit."
